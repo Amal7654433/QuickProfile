c1930ce8730a7987d5f81f7de107540cf7375e52
Amal7654433 <121153174+Amal7654433@users.noreply.github.com>
2025-02-20 16:09:29 +0530

    changes

--- Backend/server.js
+++ Backend/server.js
@@ -13,11 +13,12 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors({
-  credentials: true,
-  origin: ['http://localhost:3000',process.env.FRONTEND_URL],
+// app.use(cors({
+//   credentials: true,
+//   origin: ['http://localhost:3000',process.env.FRONTEND_URL],
 
-}));
+// }));
+app.use(cors({ origin: process.env.FRONTEND_URL }));
 
 
 
